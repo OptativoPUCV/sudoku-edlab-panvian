1f72dd8b34634defff8ce6027f0feef5cafa37a7
repl.it user <replituser@example.com>
2020-10-14 21:43:07 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -65,8 +65,10 @@ int is_valid(Node* n){
     int j=3*(k%3) + (p%3);
     printf("%d ",j);
     //printf("%d ",n->sudo[i][j]);
-    if(p%3 == 2) printf("\n");
+    //if(p%3 == 2) printf("\n");
+    printf("\n");
   }
+  printf("\n");
   return 1;
 }
 
