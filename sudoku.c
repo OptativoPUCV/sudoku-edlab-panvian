8fd3ccc21d4fb78982318e3c00c3268a3feaaebc
repl.it user <replituser@example.com>
2020-10-14 21:20:27 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -43,8 +43,30 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
-
-    return 1;
+  int i,j,k;
+  for(i=0;i<9;i++) {
+    for(j=0;j<9;j++) {
+      for(k=0;k<9;k++) {
+          if(n->sudo[i][j]==n->sudo[i][k]) {
+            if(j==k) continue;
+            return 0;
+          }
+          if(n->sudo[i][j]==n->sudo[k][j]) {
+            if(i==k) continue;
+            return 0;
+          }
+      }
+    }
+  }
+  int p; 
+  k=4;
+  for(p=0;p<9;p++){
+    int i=3*(k/3) + (p/3) ;
+    int j=3*(k%3) + (p%3) ;
+    printf("%d ",n->sudo[i][j]);
+    if(p%3 == 2) printf("\n");
+  }
+  return 1;
 }
 
 
