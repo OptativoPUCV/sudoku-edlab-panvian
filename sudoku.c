7db88355e65d661e61795f4d101af7d071bfee2c
repl.it user <replituser@example.com>
2020-10-14 22:42:05 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -92,7 +92,13 @@ List* get_adj_nodes(Node* n){
 
 
 int is_final(Node* n){
-    return 0;
+  int i,j;
+  for(i=0;i<10;i++) {
+    for(j=0;j<10;j++) {
+      if(n->sudo[i][j]==0) return 0;
+    }
+  }
+  return 1;
 }
 
 Node* DFS(Node* initial, int* cont){
