3793f30c4515b5c94d06fab6da6cf394dcd17617
repl.it user <replituser@example.com>
2020-10-14 21:25:41 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -48,7 +48,7 @@ int is_valid(Node* n){
     for(j=0;j<9;j++) {
       for(k=0;k<9;k++) {
           if(n->sudo[i][j]==n->sudo[i][k]) {
-            printf("hola");
+            //printf("hola");
             if(j!=k) return 0;
           }
           if(n->sudo[i][j]==n->sudo[k][j]) {
